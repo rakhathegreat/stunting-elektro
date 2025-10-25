e2486281f807f4b19bef9075e2f93475adc12592
rakhathegreat <rakhafadhillah516@gmail.com>
2025-10-25 14:33:05 +0700

    Still fix delete button

--- src/hooks/useSupabaseResource.ts
+++ src/hooks/useSupabaseResource.ts
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useCallback, useEffect, useRef, useState } from 'react';
+import { type Dispatch, type SetStateAction, useCallback, useEffect, useRef, useState } from 'react';
 
 const CACHE_TTL = 1000 * 60; // 1 minute
 
